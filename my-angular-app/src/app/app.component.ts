ec5177747cfcb3c1d2de63251439511dc48ecb6e
Tekla09 <166938942+Tekla09@users.noreply.github.com>
2024-04-14 21:56:03 +0400

    counter

--- my-angular-app/src/app/app.component.ts
+++ my-angular-app/src/app/app.component.ts
@@ -1,27 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CounterComponent } from './counter/counter.component';
-import { ToDoListComponent } from './to-do-list/to-do-list.component';
-
-export interface Todo {
-  name: string;
-  email: string;
-}
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CounterComponent, ToDoListComponent],
+  imports: [RouterOutlet, CounterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  users: Todo[] = [
-    { name: 'Emily Brown', email: 'emily.brown@example.com' },
-    { name: 'William Taylor', email: 'william.taylor@example.com' },
-    { name: 'Olivia Wilson', email: 'olivia.wilson@example.com' },
-  ];
-
   initialValue: number = 0;
   title = 'angular';
 
